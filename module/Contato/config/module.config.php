223e42d5fe15cd884bdca94e76ece527db4552e5
marta <marta@lmdemt.rede.local>
2013-06-12 09:06:39 -0300

    segundo commit

---
+++ module/Contato/config/module.config.php
@@ -0,0 +1,66 @@
+<?php
+
+return array(
+    # definir controllers
+    'controllers' => array(
+        'invokables' => array(
+            'HomeController' => 'Contato\Controller\HomeController',
+            'ContatosController' => 'Contato\Controller\ContatosController'
+        ),
+    ),
+
+    # definir rotas
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type'      => 'Literal',
+                'options'   => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'HomeController',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            'contatos' => array(
+                'type'      => 'segment',
+                'options'   => array(
+                    'route'    => '/contatos[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'ContatosController',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    # definir gerenciador de servicos
+    'service_manager' => array(
+        'factories' => array(
+        //    'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory',
+        ),
+    ),
+
+    # definir layouts, erros, exceptions, doctype base
+    'view_manager' => array(
+        'display_not_found_reason'  => true,
+        'display_exceptions'        => true,
+        'doctype'                   => 'HTML5',
+        'not_found_template'        => 'error/404',
+        'exception_template'        => 'error/index',
+        'template_map'              => array(
+            'layout/layout'         => __DIR__ . '/../view/layout/layout.phtml',
+            'contato/home/index'    => __DIR__ . '/../view/contato/home/index.phtml',
+            'error/404'             => __DIR__ . '/../view/error/404.phtml',
+            'error/index'           => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
